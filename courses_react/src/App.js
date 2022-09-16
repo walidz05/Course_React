b081ca2f24b69893391663832a9db23418d576a1
walidz05 <walidziani440@gmail.com>
2022-09-16 19:15:23 +0100

    config sass

--- courses_react/src/App.js
+++ courses_react/src/App.js
@@ -1,9 +1,18 @@
+import './main.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+
 function App() {
   return (
-    <div className="App">
-        main 
-    </div>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home />}/>
+         <Route path="/about" element={<About />}/>
+         <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter> 
   );
 }
-
 export default App;

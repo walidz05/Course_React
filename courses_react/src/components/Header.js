8792ed754f5d52952c9af1566b799067c3fd2e96
walidz05 <walidziani440@gmail.com>
2022-09-17 11:24:40 +0100

    header section

--- courses_react/src/components/Header.js
+++ courses_react/src/components/Header.js
@@ -1,14 +1,27 @@
-import React,{useState} from 'react';
-const Header = () => {
-
-      const [state] = useState({
-            'video' : '/assets/videos/header.mp4',
-      });
+import React,{Children, useState} from 'react';
+const Header = ({state,children}) => {
 
   return (
     <div className='header'>
       <div className="header__video">
-            <video src={state.video} autoPlay loop muted></video>
+            <video poster={state.poster} src={state.video} autoPlay loop muted></video>
+      </div>
+      <div className="header__contents">
+            <div className="container">
+                  <div className="header__contents__text">
+                        <div className="header__contents__text__child">
+                              <h1 className='header__contents__text__child__h1'>
+                              {state.heading}
+                              </h1>
+                              <p className="header__contents__text__child__p">
+                              {state.paragraph}  
+                              </p>
+                              <div className="header__contents__text__child__link">
+                                    {children}
+                              </div>
+                        </div>
+                  </div>
+            </div>
       </div>
 </div>
   )
--- courses_react/src/pages/Home.js
+++ courses_react/src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from '../components/Header';
 
 const Home = () => {
+
+     const [state,setState] = useState({
+            'video' : '/assets/videos/header.mp4',
+            'poster':'/assets/images/screen.png',
+            'heading':' we are travel friends',
+            'paragraph':"  Come and join us we travel the most famous and beatiful places in the world",
+      });
+
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header state={state}>
+            <button className='btn-default'>Get Started</button>
+      </Header>
+    </>
   )
 }
 
